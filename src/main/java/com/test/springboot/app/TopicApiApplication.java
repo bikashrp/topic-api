56a4ff94edd941b5a18c3df83fe643178ad12108
bikashrp <34394136+bikashrp@users.noreply.github.com>
2018-08-02 10:28:32 +0530

    Initial commit

---
+++ src/main/java/com/test/springboot/app/TopicApiApplication.java
@@ -0,0 +1,12 @@
+package com.test.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/test/springboot/topic/TopicApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.springboot.topic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopicApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
